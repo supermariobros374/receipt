c635cf5a40e377167d2f73ffbdd28810a899f367
supermariobros374 <supermariobros374@gmail.com>
2017-08-17 12:12:44 +0800

    Editted basic config for CMakeList.txt

---
+++ lib/CMakeFiles/online_get_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "online_get_lib.so"
+)
---
+++ src/CMakeFiles/receipt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/receipt.dir/main.cpp.o"
+  "receipt.pdb"
+  "receipt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/receipt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
